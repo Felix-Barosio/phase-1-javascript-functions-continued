da1a70628fb44b2cc3705ca1d8e14f8ea5b66278
Felix <barosiofelix@gmail.com>
2022-08-16 13:38:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(saturday = 'roller-skate'){
+    return `This Saturday, I want to ${saturday}!`
+}
+
+
+function mondayWork(activiy = "go to the office"){
+    return `This Monday, I will ${activiy}.`;
+}
+
+mondayWork()
+mondayWork("I will work from home")
+
+
+
+function wrapAdjective(special = "*"){
+    return function(innerFunction = "awesome"){
+        return `You are ${special}${innerFunction}${special}!`;
+    };
+};
+wrapAdjective()("a hard worker")
+wrapAdjective("||")("a dedicated programmer")("||")
